9ee64a82c4d8ebf797aba5ee28cbccaf0e0d498b
Oskar Herz <oskar.herz@convalid-ai.de>
2023-06-20 20:26:27 +0200

    🔧 chore: adds tailwind to the project (#3)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { styled } from 'nativewind';
+
+
+const StyledText = styled(Text);
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Yeet</Text>
+      <StyledText className="font-bold">
+        Open up App.js to start working on your app!
+      </StyledText>
       <StatusBar style="auto" />
     </View>
   );
